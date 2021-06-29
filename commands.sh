dd50085419504b2cd9aae0a2dc416c336b06a6d7
ant0n <69129717+antonchu2006@users.noreply.github.com>
2021-06-29 05:00:26 -0700

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,18 @@
+# This are the commands used in this video
+# https://www.youtube.com/watch?v=8utpbbdj0LQ
+
+cfdisk # select dos and create a single partition
+
+mkfs.xfs /dev/sda1
+
+mount /dev/sda1 /mnt
+
+pcman -Sy tmux
+
+tmux
+
+pcstrap /mnt base linux
+
+## Create another window with Ctrl + b and "
+
+genfstab /mnt>/mnt&etc/fstab;arch-chroot /mnt bash -c 'pacman -Sy grub;grub-install /dev/sda;grub-mkconfig -o /boot/grub/grub.cfg';reboot
